6dcc9c0088eb7ba7370374147cd9c7a6bbe3e380
Darman Sahputra Harefa <66565894+Putra-28@users.noreply.github.com>
2022-07-14 05:38:50 +0700

    Add files via upload

---
+++ jurnal.py
@@ -0,0 +1,22 @@
+import datetime
+import os
+
+os.system('clear')
+x = datetime.datetime.now()
+
+print ('''
+     SILAHKAN TULIS KEGIATAN ANDA(☞ﾟ∀ﾟ)☞
+         JANGAN LUPA TERSENYUM KAWAN
+              BY.MONKEY D DARMAN
+
+''')
+f = open("jurnal.txt", 'a')
+while True:
+  msg = input("Kegiatan ☞ ")
+  tgl = x.strftime("[%d,%B][%H.%M] ")
+  if msg == '':
+    f.write("\n \n \n")
+    break
+  f.write(tgl+msg+"\n")
+
+f.close()
