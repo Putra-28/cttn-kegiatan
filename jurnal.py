0210e381b2f233af722b1bd674add97b2ce9cc47
Darman Sahputra Harefa <66565894+Putra-28@users.noreply.github.com>
2022-07-18 09:44:00 +0700

    update jurnal.py

--- jurnal.py
+++ jurnal.py
@@ -1,24 +1,35 @@
 import datetime
 import os
 
-os.system('clear')
 x = datetime.datetime.now()
+f = open("jurnal.txt", 'a')
 
+
+os.system('clear')
 print ('''
      SILAHKAN TULIS KEGIATAN ANDA(☞ﾟ∀ﾟ)☞
          JANGAN LUPA TERSENYUM KAWAN
               BY.MONKEY D DARMAN
 
-!!Klik Enter Saja Jika Ingin Menyelesaikan!!
+1. Add Kegiatan
+2. Lanjutan
 
 ''')
-f = open("jurnal.txt", 'a')
-while True:
-  msg = input("Kegiatan ☞ ")
-  tgl = x.strftime("[%d,%B][%H.%M] ")
-  if msg == '':
-    f.write("\n \n \n")
-    break
-  f.write(tgl+msg+"\n")
+menu = input("=> ") 
 
-f.close()
+if menu == '1':
+  t = x.strftime("%d, %B")
+  f.write("\n"+t)
+  ruangan = input("Ruangan ☞ ")
+  msg = input("Kegiatan ☞ ")
+  tgl = x.strftime(f"\n[{ruangan}][%d,%B][%H.%M] ")
+  f.write(tgl+msg+"\n")
+  
+elif menu == '2':
+  ruangan = input("Ruangan ☞ ")
+  msg = input("Kegiatan ☞ ")
+  tgl = x.strftime(f"[{ruangan}][%d,%B][%H.%M] "+msg)
+  f.write(tgl+"\n")
+  
+else:
+  print("Wrong System")
